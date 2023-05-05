51851994ef67aa3cbb2c887a16c270a9412b7117
Henry Tu <67282986+HenryTu2912@users.noreply.github.com>
2023-05-05 10:32:49 -0600

    Finished the project

--- Garage management in C/cmake-build-debug/cmake_install.cmake
+++ Garage management in C/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/hungt/Desktop/tu4422ho4847cosc292a2
+# Install script for directory: D:/GitHub/C-Assignment/Garage management in C
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/hungt/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/223.8836.42/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/hungt/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/hungt/Desktop/tu4422ho4847cosc292a2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/GitHub/C-Assignment/Garage management in C/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
